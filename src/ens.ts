0cc7ce4f0d488714cc2d0c38f0859456b88733b9
Makoto Inoue <2630+makoto@users.noreply.github.com>
2021-06-06 18:52:21 +0100

    Update ENS

--- src/ens.ts
+++ src/ens.ts
@@ -1,3 +1,6 @@
+const gr = require('graphql-request')
+const { request, gql } = gr
+const url = 'https://api.thegraph.com/subgraphs/name/ensdomains/ens'
 require('dotenv').config()
 const argv = require('yargs/yargs')(process.argv.slice(2))
   .option('user', {
@@ -7,8 +10,21 @@ const argv = require('yargs/yargs')(process.argv.slice(2))
 
 const fs = require('fs');
 import _ from 'lodash'
-import { getOrFetch, fetchTransactionCount } from './utils'
+import { getEnsData, getOrFetch, fetchTransactionCount } from './utils'
 import { ethers } from 'ethers';
+
+const REGISTRATIONS_SUBGRAPH_QUERY = gql`
+  query Registrations($labelNames: [String]) {
+    registrations(orderBy:registrationDate, orderDirection:desc, where:{labelName_in:$labelNames}){
+      id
+      labelName
+      registrationDate
+      registrant{
+        id
+      }
+    }
+  }
+`
 
 export const main = async () => {
   let addresses
@@ -29,25 +45,61 @@ export const main = async () => {
   }
 
   console.log({addresses:addresses.length})
-  let results = []
-  let chains = {
-    ethereum:0,
-    polygon: 0,
-    avalanche: 0,
-    bsc: 0,
-    fantom: 0,
-    xdai:0
-  }
-  const transactions = []
-  for (let index = 0; index < addresses.length; index++) {
-  // for (let index = 0; index < 3; index++) {
-    const address = addresses[index];    
-
-    const ethereum = (await getOrFetch(`./data/transactions/${1}-${address}.json`, fetchTransactionCount, [address, 1]))
-    transactions.push(ethereum)
-    console.log(`${address} , ${ethereum.length}`)
-  }
-
+//   const transactions = []
+//   for (let index = 0; index < addresses.length; index++) {
+//   // for (let index = 0; index < 3; index++) {
+//     const address = addresses[index];    
+//     const ethereum = (await getOrFetch(`./data/transactions/${1}-${address}.json`, fetchTransactionCount, [address, 1]))
+//     transactions.push(ethereum)
+//     console.log(`${address} , ${ethereum.length}`)
+//   }
+    const limit = 50
+    const chunked = _.chunk(addresses, limit)
+    const voters = {}
+    const spaces = {}
+    const reverseNames = {}
+    const result = []
+    for (let i = 0; i < chunked.length; i++) {
+      // for (let i = 0; i < 2; i++) {
+      const element = chunked[i];
+      // console.log(i, element.length)
+      const ensNames = await getEnsData(element)
+      
+      // for (let j = 0; j < element.length; j++) {
+      //   const address = element[j];
+      //   const name = ensNames[j];
+      //   if(ensNames[j] !== ''){
+      //     reverseNames[address] = name
+      //   }
+      // }
+      const labelNames = ensNames.map(e => e.split('.')[0] )
+      // console.log({labelNames})
+      let { registrations } = await request(url, REGISTRATIONS_SUBGRAPH_QUERY, {
+        labelNames
+      })
+      registrations.map(r => {
+        const string =  `${r.registrant.id} , ${r.labelName}.eth , ${r.registrationDate}`
+        result.push(string)
+      })
+      // for (let j = 0; j < result.votes.length; j++) {
+      //   const vote = result.votes[j];
+      //   const voter = reverseNames[vote.voter] || vote.voter
+      //   if(voters[voter]){
+      //   voters[voter].push(vote)
+      //   }else{
+      //   voters[voter] = [vote]
+      //   }
+      //   if(spaces[vote.space.id]){
+      //   if(vote.space.id === 'rallygov.eth'){
+      //       console.log({vote})
+      //   }
+      //   spaces[vote.space.id].push(vote)
+      //   }else{
+      //   spaces[vote.space.id] = [vote]
+      //   }
+      // }
+    }
+    console.log(result)
 }
  
 console.log(argv)
